2cf7bb3edbe77807bbd33b696174de89d24eaca5
„Pavla <zimmermannovap@gmail.com>
2026-04-09 13:01:08 +0200

    fix sitemap URL: use ng-consulting.vercel.app until custom domain is live

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: process.env.NEXT_PUBLIC_SITE_URL || 'https://ngconsulting.cz',
+  siteUrl: process.env.NEXT_PUBLIC_SITE_URL || 'https://ng-consulting.vercel.app',
   generateRobotsTxt: true,
   exclude: ['/icon', '/opengraph-image'],
   robotsTxtOptions: {
